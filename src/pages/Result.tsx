c5348a8f9979bc80c085b1ddd58bf24899988c16
Gad Nadjar <gadnadjar@gmail.com>
2025-01-14 15:55:47 +0200

    add new events

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,36 +1,64 @@
+import { FormEvent, ReactElement, useState } from 'react';
 import { assets } from '../assets/assets';
+import { FormEncType } from 'react-router-dom';
 
 const Result = () => {
+
+    const [image, setImage] = useState<string>(assets.sample_img_1);
+    const [isImageLoaded, setIsImageLoaded] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [input, setInput] = useState<string>('');
+
+    const onSubmitHandler = async (e: FormEvent) => {
+
+    };
+
+    console.log(input);
+
     return (
-        <form className='flex flex-col min-h-[90vh] justify-center items-center'>
+        <form onSubmit={onSubmitHandler} className='flex flex-col min-h-[80.5vh] justify-center items-center'>
             <div>
                 <div className='relative'>
                     <img className='max-w-sm rounded'
-                        src={assets.sample_img_1} alt="" />
-
-                    <span className='absolute botton-0 left-0 h-1 bg-blue-400 w-full transition-all duration-[10s]' />
+                        src={image} alt="" />
+                    <span className={`absolute botton-0 left-0 h-1 bg-blue-500 
+                        ${loading ? ' w-full transition-all duration-[10s]' : 'w-0'}`} />
                 </div>
 
-                <p>Loading.....</p>
+                <p className={!loading ? 'hidden' : ''}>Loading.....</p>
             </div>
 
-            <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
-                <input
-                    className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 '
-                    type="text"
-                    placeholder='Describe what you want to generate' />
+            {!isImageLoaded && (
+                <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
+                    <input
+                        onChange={(e) => setInput(e.target.value)}
+                        value={input}
+                        className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 '
+                        type="text"
+                        placeholder='Describe what you want to generate' />
 
-                <button
-                    className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'
-                    type='submit'>Generate
-                </button>
-            </div>
+                    <button
+                        className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'
+                        type='submit'>Generate
+                    </button>
+                </div>
+            )}
 
-            <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
-                <p>Generate Another</p>
-                <a href=''>Download</a>
-            </div>
-
+            {isImageLoaded && (
+                <div className='flex gap-2 flex-wrap justify-center items-center text-white text-sm p-0.5 mt-10 rounded-full'>
+                    <p
+                        onClick={() => { setIsImageLoaded(false); }}
+                        className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>
+                        Generate Another
+                    </p>
+                    <a
+                        download
+                        className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'
+                        href={image}>
+                        Download
+                    </a>
+                </div>
+            )}
         </form>
     );
 };
